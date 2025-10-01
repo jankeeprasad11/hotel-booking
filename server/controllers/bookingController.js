d0b38ea8355502b8734b4484c8017a42d656e523
jankeeprasad11 <jankeeprasad11@gmail.com>
2025-10-01 13:14:28 +0530

    project completed 2

--- server/controllers/bookingController.js
+++ server/controllers/bookingController.js
@@ -122,7 +122,7 @@ export const getHotelBookings = async (req, res)=>{
         if(!hotel){
             return res.json({success: false, message: "No Hotel found"})
         }
-        const bookings = (await Booking.find({hotel: hotel._id}).populate("room hotel user")).toSorted({createdAt: -1});
+        const bookings = await Booking.find({hotel: hotel._id}).populate("room hotel user").sort({createdAt: -1});
         //Total Bookings
         const totalBookings = bookings.length;
         //Total Revenue
